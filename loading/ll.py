93a1665a8b9fd7e186aab83609fcf384e75a9066
Ankit <ankit19200@gmail.com>
2021-05-27 18:47:57 +0530

    add vehicle type in ll.py

--- loading/ll.py
+++ loading/ll.py
@@ -16,18 +16,102 @@ class LL(Load):
 		super(LL, self).__init__('ll',name)
 
 
-		if 'load_on_bearings' in kwargs.keys():
-			self.load_summary[self.load_type][self.name] = kwargs['load_on_bearings']
+		### set default vehicle ###
+		self.vehicle = {}
+		self.default_vehicle()
 
 
 
-	def calculate_LL(self):
+
+
+	def calculate_LL(self,vehicle = ""):
 		''' tool to calculate superimposed dead load on beaarigns'''
 
-		# self.load_summary[self.load_type][self.name] = 'calcualted dead load'
+		# self.load_summary[self.load_type][self.name] = 'calcualted live load'
 		pass
 
-d = LL(name = 'tere naam')
-print (d.load_type)
-print(d.name)
-print(d.load_summary)
+
+
+
+
+
+
+
+
+
+
+	def define_vehicle(self,name="",axle_load =[],distance=[],longitudenal_clearance=100, transverse_clearance = [100,100],width = 2.3,tyre_width=0.5):
+
+		self.vehicle[name] = {'axle_load':axle_load,'axle_distance':distance,'longitudenal_clearance' : longitudenal_clearance,'transverse_clearance': transverse_clearance,'width': width,'tyre_width' : tyre_width}
+
+
+	def default_vehicle(self):
+		
+
+		''' modify above code based on code'''
+		width = min(self.superstructure_details['left']['width'] ,self.superstructure_details['right']['width'])
+		self.vehicle['classA'] = {'axle_load':[2.7, 2.7, 11.4, 11.4, 6.8, 6.8, 6.8, 6.8],'axle_distance':[0, 1.1, 3.2, 1.2, 4.3, 3, 3, 3],'longitudenal_clearance':20,'transverse_clearance':[0.15, 0.4  if (width <5.3) else min(0.4 + width-5.3,1.2)],'width':2.3,'tyre_width':0.5}
+		self.vehicle['70R'] = {'axle_load':[17, 17, 17, 17, 12, 12, 8],'axle_distance':[0, 1.37, 3.05, 1.37, 2.13, 1.52, 3.96],'longitudenal_clearance':20,'transverse_clearance':[1.2,1.2],'width':2.79,'tyre_width':0.86}
+
+
+	def reaction_calculator (self,vehicle_type,*args):
+		''' calculate reaction Ra and Rb of individual strucutres
+			parameters are :
+			args[0] = details of 1st span as per required parameters in influence line + expension gap :
+			args[1] = details of 2nd span as per required parameters in influence line
+		'''
+		vehicle = self.vehicle[vehicle_type]
+		spans = [x[0] for x in  args]
+		exp_gap = [x[-1] for x in args[:-1]]
+		x_total = sum(spans) + sum(exp_gap)
+		no_of_vehicle = int (2+ x_total /sum(vehicle['axle_distance']))
+		axle = vehicle['axle_load']*no_of_vehicle
+		distance = vehicle['axle_distance']*no_of_vehicle
+
+		
+
+
+
+	def influence_line(self,x,*args,**kwargs):
+		''' calculate influence line of structures
+			parameters:
+			for simply supported :
+				args[0]		: overall span 
+				args[1]		: effective span 
+				args[2]		: left overhang (optional)
+			for continuous:
+				args[0]		: overall span 
+				args[1]		: effective span 
+				args[2]		: left overhang (optional)
+				args[4]		: span 1
+				args[5]		: span 2
+				args[6]		: span 3
+				...
+			for cantelever:
+				args[0]		: overall span
+				args[1]		: left/right
+			bridge_type : continuous, simply_supported, cantelever
+			'''
+		if kwargs['bridge_type'] in ['ss', 'simply_supported']:
+			os = args[0]
+			es = args[1]
+			if len(args) == 3:
+				lo = args[2]
+			else:
+				lo = 0
+
+			a = lo if lo != 0 else 0.5*(os-es)
+			b = os-es-lo
+			return ( 1-(x-a)/es, (x-a)/es )
+
+		elif kwargs['bridge_type'] in ['cantelever','cant']:
+			if args[1] == 'left':
+				return (1,0)
+			else :
+				return (0,1)
+		elif kwargs['bridge_type'] in ['continuous', 'c'] :
+			# to be implemented
+			pass
+
+
+
--- loading/load.py
+++ loading/load.py
@@ -2,7 +2,7 @@ class Load:
 	"""docstring for Load"""
 	load_summary = {'dl':{},'sidl':{},'ll':{}}
 	bearing_details = {'bearing_nodes':[], 'bearing_coordinates':[],'bearing_depth':0}
-	superstructure_details = {'left':{'effective':0,'overall':0,'clear':0,'depth':0,'width':0},'right':{'effective':0,'overall':0,'clear':0,'depth':0,'width':0}}
+	superstructure_details = {'left':{'effective':0,'overall':0,'clear':0,'depth':0,'width':2*3.5},'right':{'effective':0,'overall':0,'clear':0,'depth':0,'width':2*3.5},'expension_gap':0.1}
 
 	def __init__(self,load_type,name=""):
 		if load_type in list(self.load_summary.keys()):
@@ -26,13 +26,27 @@ class Load:
 		self.bearing_details['bearing_coordinates'] = kwargs['bearing_coordinates']
 
 	def set_superstructure_details(self,**kwargs):
-		pass
+		self.superstructure_details['left'] = kwargs['left']
+		self.superstructure_details['right'] = kwargs['right']
+		self.superstructure_details['expension_gap'] = kwargs['expension_gap']
+
 	def set_supersturcture_details_left(self,**kwargs):
-		pass
+		self.superstructure_details['left']['effective'] = kwargs['effective'] 
+		self.superstructure_details['left']['overall'] = kwargs['overall'] 
+		self.superstructure_details['left']['clear'] = kwargs['clear'] 
+		self.superstructure_details['left']['depth'] = kwargs['depth'] 
+		self.superstructure_details['left']['width'] = kwargs['width'] 
+
 	def set_supersturcture_details_right(self,**kwargs):
-		pass
+		self.superstructure_details['right']['effective'] = kwargs['effective'] 
+		self.superstructure_details['right']['overall'] = kwargs['overall'] 
+		self.superstructure_details['right']['clear'] = kwargs['clear'] 
+		self.superstructure_details['right']['depth'] = kwargs['depth'] 
+		self.superstructure_details['right']['width'] = kwargs['width'] 
 
 
+	def set_load(self,load_on_bearing):
+		self.load_summary[self.load_type][self.name] = load_on_bearing
 
 	def get_loads(self,name=''):
 		''' common output format for all child class '''
